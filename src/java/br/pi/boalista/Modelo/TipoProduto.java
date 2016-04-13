e193e88611434c5fb899f108c0967479c98f6d21
Luciano <Luciano@10.28.3.177>
2016-04-13 20:33:35 -0300

    Primeiro comit

---
+++ src/java/br/pi/boalista/Modelo/TipoProduto.java
@@ -0,0 +1,68 @@
+
+package br.pi.boalista.Modelo;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+
+@Entity(name="tipoproduto")
+public class TipoProduto implements Serializable {
+    
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column
+    private String nome;
+    @Column
+    private double valor;
+    
+    
+    
+    
+
+    public TipoProduto(String nome, double valor) {
+        this.nome = nome;
+        this.valor = valor;
+        
+    }
+
+  
+
+    public TipoProduto() {
+    }
+    
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+   
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+   
+}
